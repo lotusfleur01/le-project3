9a50084cf9b1103e27a46e654974d640b2a436e5
Erika Montijo <erikamontijo@erikas-mbp.attlocal.net>
2020-05-31 12:18:58 -0500

    I completed the exercise on this branch

---
+++ README.md
@@ -0,0 +1,4 @@
+Lightning Exercise 3: 
+
+Question: Define an object for four family members, and put each object in an array named familyMembers. Each family member object that you create should have the same keys on them, but the values will be different.
+
---
+++ main.js
@@ -0,0 +1,38 @@
+// var familyMembers = [ 'Erika', 'Dash', 'Bitsy', 'Neal' ];
+
+// const favoriteTreat = {
+//     Neal: "biscuits",
+//     Erika: "pie",
+//     Dash: "blueberries",
+//     Bitsy: "steak"
+// }
+
+const Neal = {
+    favoriteTreat: "biscuits",
+    favoriteColor: "green",
+    favoriteHobby: "gaming",
+}
+
+ const Erika = {
+    favoriteTreat: "wine",
+    favoriteColor: "peony pink",
+    favoriteHobby: "reading",
+ }
+
+ const Bity = {
+    favoriteTreat: "steak",
+    favoriteColor: "gray",
+    favoriteHobby: "barking",
+ }
+
+ const Dash = {
+    favoriteTreat: "blueberries",
+    favoriteColor: "blue",
+    favoriteHobby: "sleeping",
+ }
+
+var familyMembers = [ "Neal", "Erika", "Bitsy", "Dash"]
+
+console.log (Erika);
+console.log (familyMembers);
+console.log(familyMembers[1]);
